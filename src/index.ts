e480c89107d6e04d058acea987c85eaf7ba4aeeb
Bekhruz Rakhmonov <bekhruzrakhmonov1@gmail.com>
2023-10-21 00:49:23 +0200

    custom port changed

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import exceptionFilter from './middleware/exception'
 dotenv.config()
 
 const app: Express = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 6060
 
 const v1Router = express.Router()
 
